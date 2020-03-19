3b783106ef5fdad8022c2187cad914badf776e2d
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-03-19 16:58:21 +0900

    url파라미터의 값에 따라 프로그래밍 적으로 제목 생성

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
-<!doctype html>
-<html>
-  <body>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <h1>WEB</h1>
+  <ol>
+    <li><a href="index.php?id=HTML">HTML</a></li>
+    <li><a href="index.php?id=CSS">CSS</a></li>
+    <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+  </ol>
+  <!-- url에 아이디 값에 따라 현재 페이지의 제목 정하기 -->
+  <h2>
     <?php 
-    // 현재의 시간을 출력해라
-      echo date('Y-m-d H:i:s');
-    ?> 
-  </body>
+      echo $_GET['id'];
+    ?>
+  </h2>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum doloremque quas expedita incidunt neque animi a. Nisi corporis ratione, quaerat repellendus, explicabo assumenda suscipit mollitia sapiente nulla magni vel nesciunt.
+</body>
 </html>
