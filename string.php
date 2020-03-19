bc8acdf1bb304c573a737110dd7ed9c80ca5c71e
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-03-19 16:45:59 +0900

    php

---
+++ string.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+  <body>
+    <h1>String & String Operator</h1>
+    <?php 
+      echo "Hello \"w\"orld";
+    ?>
+    <!-- 문자열 관련 연산자 -->
+    <h2>concatenation Operator</h2>
+    <?php 
+      echo "Hello "."world";
+    ?>
+    <!-- 글자 수 알아내는 함수 -->
+    <h2>String length function</h2>
+    <?php 
+      echo strlen("Hello world");
+    ?>
+  </body>
+</html>
---
+++ variable.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Variable</h1>
+    <?php
+    $name = "hana";
+    // 앞에있는 문자와 변수를 결합해야해서 . 이라는 결합 연산자를 사용함 
+    echo  "Lorem ipsum dolor sit amet ".$name." consectetur adipisicing elit. ".$name." Eius expedita vel eos quo accusantium consequuntur inventore, provident sunt officiis soluta veritatis ".$name." deleniti voluptatem cum doloribus porro ".$name." laborum repudiandae modi a!
+    Lorem ipsum dolor sit amet ".$name." consectetur adipisicing elit. ".$name." Eius expedita vel eos quo accusantium consequuntur inventore, provident sunt officiis soluta veritatis ".$name." deleniti voluptatem cum doloribus porro ".$name." laborum repudiandae modi a!";
+    ?>
+  </body>
+</html>
