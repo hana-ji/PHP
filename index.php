554072b5630f9b63265ab26edc0942acc5e7d228
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-03-20 14:31:03 +0900

    조건문

--- index.php
+++ index.php
@@ -5,23 +5,36 @@
   <title>Document</title>
 </head>
 <body>
-  <h1>WEB</h1>
+  <h1><a href="index.php">WEB</a></h1>
   <ol>
     <li><a href="index.php?id=HTML">HTML</a></li>
     <li><a href="index.php?id=CSS">CSS</a></li>
     <li><a href="index.php?id=JavaScript">JavaScript</a></li>
   </ol>
-  <!-- url에 아이디 값에 따라 현재 페이지의 제목 정하기 -->
   <h2>
-    <?php 
+    <!-- 만약에 id값이 있다면
+          echo $_GET['id']; 를 실행시키고
+         없다면
+          echo "Welcome"; 을 실행시킨다 -->
+    <?php
+    // $_GET[id]의 값이 isset(있다면) true가 됨 값이 없다면 if의 값이 false가 되서 else에 있는 부분이 출력됨
+    if(isset($_GET['id'])){
       echo $_GET['id'];
+    } else {
+      echo "Wellcome";
+    }
     ?>
   </h2>
   <?php 
-    // echo data/id 값에 해당하는 파일의 내용;
+  // $_GET['id'가 있다면(true)
+  if(isset($_GET['id'])){
+    // 여기가 실행될것이고
     echo file_get_contents("data/".$_GET['id']);
-    /* file_get_contents라는 함수를 통해서 id값으로 들어오는 값으로 프로그래밍 적으로 php를 통해서 경로로 전환해준 다음에 그것을 file_get_contents에 입력값으로 전해주면
-    그 입력값으로 들어온 경로에 해당되는 파일을 찾아서 표현해주는 표현식이다 */
+    // 없다면(false)
+  } else {
+    // 여기가 실행될것이다
+    echo "Hello, PHP";
+  }
   ?>
     
 </body>
