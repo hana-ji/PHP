166959318486a9a151c228459878a783c024e5c9
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-03-23 17:02:28 +0900

    함수 출력값

--- function2.php
+++ function2.php
@@ -12,20 +12,28 @@
       print("agljmlgbjkasbdgjabsdgsd1<br>");
       print("agljmlgbjkasbdgjabsdgsd2<br>");
     }
-    // ()안에 입력값을 주면 입력값을 받아 내부적으로 다르게 동작하게 할수 있음
     basic();
     basic();
   ?>
   <h2>parameter &amp; argument</h2>
   <?php
-  // sum이라는 함수를 만듬, 그 함수의 입력값은 $left, $right 라고 하는 두개의 값이 올수있음
-  // $left, $right = parameter(매개변수)
-  function sum($left, $right){
-    print($left+$right);
-  }
-  // 입력값을 뭘 주냐에 따라 다르게 동작
-  // 2,4 = argument
-    sum(2,4);
+  // sum: ($legt, $right)에 입력값을 들어온 값을 더한다, print 더한 값을 출력한다 
+    function sum($left, $right){
+      print($left+$right);
+      print("<br>");
+      // 출력한다 라는 기능이 내장되있어서 쓸모 적음
+    }
+      sum(2,4);
+  ?>
+  <!-- 함수의 출력값: return -->
+  <h2>return</h2>
+  <?php
+    // 동작한 결과를 리턴(출력)하기 때문에 다양한 용도로 사용 가능
+    function sum2($left, $right){
+      return $left+$right;
+    }
+    // 실행 시 2+4 를 더한 결과인 6이 되는 표현식
+    print(sum2(2,4));
   ?>
 </body>
 </html>
