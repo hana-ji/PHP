0fc9b8844c4c1532f7d3f3e3487107ee7e3b5b31
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-03-23 15:39:05 +0900

    함수의 입력값

--- function2.php
+++ function2.php
@@ -12,8 +12,20 @@
       print("agljmlgbjkasbdgjabsdgsd1<br>");
       print("agljmlgbjkasbdgjabsdgsd2<br>");
     }
-    
+    // ()안에 입력값을 주면 입력값을 받아 내부적으로 다르게 동작하게 할수 있음
     basic();
+    basic();
+  ?>
+  <h2>parameter &amp; argument</h2>
+  <?php
+  // sum이라는 함수를 만듬, 그 함수의 입력값은 $left, $right 라고 하는 두개의 값이 올수있음
+  // $left, $right = parameter(매개변수)
+  function sum($left, $right){
+    print($left+$right);
+  }
+  // 입력값을 뭘 주냐에 따라 다르게 동작
+  // 2,4 = argument
+    sum(2,4);
   ?>
 </body>
 </html>
